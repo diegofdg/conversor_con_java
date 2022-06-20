ddbd30b0edfb85f870ae39de43b11a026895d185
diegofdg <diegofdg@gmail.com>
2022-06-20 07:23:28 -0300

    comenzando con el proyecto

---
+++ src/com/alura/modelo/Procesos.java
@@ -0,0 +1,16 @@
+package com.alura.modelo;
+
+public class Procesos {
+	public boolean ValidarNumero(String input) {
+		System.out.println("Validando "+input);		
+		return true;
+	}
+
+	public void convertir(double input) {
+		System.out.println("Convirtiendo moneda "+input);		
+	}
+
+	public void convertirTemperatura(double input) {
+		System.out.println("Convirtiendo temperatura "+input);		
+	}
+}
